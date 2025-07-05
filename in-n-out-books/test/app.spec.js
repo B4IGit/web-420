ccfe573286ae86f1e1571d37ae8785144be10a9e
Devin Ledesma <dev.dfledesma@gmail.com>
2025-07-05 17:14:33 -0500

    created tests for chapter 4. Tests the status code for successfully adding a new book (201), verifies key are not missing or have to many (400), and verifies the success of deleting a book (204)

--- in-n-out-books/test/app.spec.js
+++ in-n-out-books/test/app.spec.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 const request = require('supertest');
 
-describe('In-N-Out, JSON Web Server', () => {
+describe('Chapter 3: API Tests', () => {
   it ('should return an array of books', async () => {
     const res = await request(app).get('/api/books');
     expect(res.statusCode).toEqual(200);
@@ -29,4 +29,32 @@ describe('In-N-Out, JSON Web Server', () => {
     expect(res.statusCode).toEqual(400);
     expect(res.body.message).toEqual('Input must be a number');
   });
-});
+}); // End chapter 3: API Tests
+
+describe('Chapter 4: API Tests', () => {
+  it('should return a 201 status code when adding a new recipe', async () => {
+    const res = await request(app).post('/api/books').send({
+      id: 99,
+      title: 'If It Bleeds',
+      author: 'Stephen King',
+    });
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it('should return a 400 status code when adding a new book with missing author', async () => {
+    const res = await request(app).post('/api/books').send({
+      id: 100,
+      title: 'If It Bleeds'
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.message).toEqual('Bad Request');
+  });
+
+  it('should return a 204 status code when deleting a book', async  () => {
+    const res = await request(app).delete('/api/books/99');
+
+    expect(res.statusCode).toEqual(204);
+  });
+}); // End chapter 4: API Tests
